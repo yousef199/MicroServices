d4ef782627ec7573713e7c95e1404923ac3c99ee
yousef <yousef.yaser@gmail.com>
2022-06-10 22:38:33 +0300

    addes spring data jpa and finished the entity and repo classes for the customer

--- Customer/src/main/java/com/yousef/customer/CustomerService.java
+++ Customer/src/main/java/com/yousef/customer/CustomerService.java
@@ -1,14 +1,19 @@
 package com.yousef.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest){
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName)
                 .lastName(customerRegistrationRequest.lastName)
                 .email(customerRegistrationRequest.email)
                 .build();
+        customerRepository.save(customer);
     }
+
 }
