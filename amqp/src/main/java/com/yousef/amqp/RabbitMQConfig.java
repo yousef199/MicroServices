45740b046e0dfc6c8eb2dbc020c8c8fef0682a83
yousef <yousef.yaser@gmail.com>
2022-07-29 20:18:44 +0300

    added rabbit mq

---
+++ amqp/src/main/java/com/yousef/amqp/RabbitMQConfig.java
@@ -0,0 +1,28 @@
+package com.yousef.amqp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQConfig {
+    private final ConnectionFactory connectionFactory;
+
+    @Bean
+    public AmqpTemplate amqpTemplate(){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter jacksonConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+}
