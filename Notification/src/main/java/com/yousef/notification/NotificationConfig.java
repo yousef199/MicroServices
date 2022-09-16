c8d27e708fddd94986e5b9f210ff83bc9105d8ec
yousef <yousef.yaser@gmail.com>
2022-09-16 21:25:30 +0300

    added rabbitmq queue and exchange config to notification service

---
+++ Notification/src/main/java/com/yousef/notification/NotificationConfig.java
@@ -0,0 +1,46 @@
+package com.yousef.notification;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NotificationConfig {
+    @Value("${rabbitmq.exchanges.internal}")
+    private String internalExchange;
+
+    @Value("${rabbitmq.queues.notification}")
+    private String notificationQueue;
+
+    @Value("${rabbitmq.routing-keys.internal-notification}")
+    private String routingKeys;
+
+    @Bean
+    public TopicExchange internalTopicExchange(){
+        return new TopicExchange(this.internalExchange);
+    }
+    @Bean
+    public Queue notificationQueue(){
+        return new Queue(this.notificationQueue);
+    }
+    @Bean
+    public Binding internalToNotificationQueue(){
+        return BindingBuilder.bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(this.routingKeys);
+    }
+    public String getInternalExchange() {
+        return internalExchange;
+    }
+    public String getNotificationQueue() {
+        return notificationQueue;
+    }
+
+    public String getRoutingKeys() {
+        return routingKeys;
+    }
+}
--- amqp/src/main/java/com/yousef/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/com/yousef/amqp/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.yousef.amqp;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -15,12 +16,18 @@ public class RabbitMQConfig {
     private final ConnectionFactory connectionFactory;
 
     @Bean
-    public AmqpTemplate amqpTemplate(){
+    public AmqpTemplate amqpTemplate(){ // Send messages to the queue
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jacksonConverter());
         return rabbitTemplate;
     }
-
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(){ // receive messages from the queue
+        SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory = new SimpleRabbitListenerContainerFactory();
+        simpleRabbitListenerContainerFactory.setConnectionFactory(connectionFactory);
+        simpleRabbitListenerContainerFactory.setMessageConverter(jacksonConverter());
+        return simpleRabbitListenerContainerFactory;
+    }
     @Bean
     public MessageConverter jacksonConverter(){
         return new Jackson2JsonMessageConverter();
