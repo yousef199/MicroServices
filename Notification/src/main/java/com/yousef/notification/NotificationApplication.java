30c9f08c65afc9c8d49a1374be2144ee149d1a32
yousef <yousef.yaser@gmail.com>
2022-09-30 10:52:14 +0300

    latest changes

--- Notification/src/main/java/com/yousef/notification/NotificationApplication.java
+++ Notification/src/main/java/com/yousef/notification/NotificationApplication.java
@@ -1,13 +1,26 @@
 package com.yousef.notification;
 
+import com.yousef.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.yousef.notification",
+                "com.yousef.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class , args);
+    }
+
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer rabbitMQMessageProducer , NotificationConfig notificationConfig){
+        return args -> {
+            rabbitMQMessageProducer.publish("foo" , notificationConfig.getInternalExchange() , notificationConfig.getRoutingKeys());
+        };
     }
 }
